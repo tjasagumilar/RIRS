df47a94db13fb611fdd012b12c3a5b81b98e4013
vitoopolenek <vito.polenek30@gmail.com>
2025-01-15 19:01:59 +0100

    Popravek

--- aplikacija/budgetbackend/init.sql
+++ aplikacija/budgetbackend/init.sql
@@ -1,3 +1,10 @@
+-- Create the database if it doesn't exist
+CREATE DATABASE budget_db;
+
+-- Connect to the database
+\c budget_db
+
+-- Create the table
 CREATE TABLE IF NOT EXISTS budgets (
     id SERIAL PRIMARY KEY,
     department VARCHAR(255) NOT NULL,
