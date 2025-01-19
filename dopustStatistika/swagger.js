dc1c1bb94950dadeab0945b921f66676c6574ae1
tjasag <tjasa.gumilar@student.um.si>
2025-01-19 14:49:10 +0100

    Dopusti API

---
+++ dopustStatistika/swagger.js
@@ -0,0 +1,25 @@
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+// Swagger konfiguracija
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Dopusti API',
+      version: '1.0.0',
+      description: 'API za upravljanje in statistiko dopustov',
+    },
+    servers: [
+      {
+        url: 'http://127.0.0.1:5003',
+        description: 'Lokalni strežnik',
+      },
+    ],
+  },
+  apis: ['./index.js'],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+module.exports = { swaggerUi, swaggerDocs };
